c472f572623970a3304ca32650d0afd6d4412996
David Chamorro <daved3464@gmail.com>
2023-01-25 09:54:18 -0500

    throw exception if sign position page is not at least 1

--- src/Document/SignProfilePosition.php
+++ src/Document/SignProfilePosition.php
@@ -4,6 +4,7 @@ namespace Hollow3464\AucoHelper\Document;
 
 class SignProfilePosition
 {
+    /** @throws \Exception */
     public function __construct(
         public readonly int $page,
         public readonly int $x,
@@ -12,5 +13,8 @@ class SignProfilePosition
         public readonly int $h
     )
     {
+        if ($page <= 0){
+            throw new \Exception("The page must be at least 1");            
+        }
     }
 }
